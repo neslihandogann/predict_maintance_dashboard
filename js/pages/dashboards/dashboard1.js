51f34c20b0cf1e585c2764f35d6e8df9c8096810
forevercodergirl/forever <75863951+neslihandogann@users.noreply.github.com>
2023-05-07 17:07:33 +0300

    Add files via upload

---
+++ js/pages/dashboards/dashboard1.js
@@ -0,0 +1,85 @@
+/*
+Template Name: Admin Pro Admin
+Author: Wrappixel
+Email: niravjoshi87@gmail.com
+File: js
+*/
+$(function () {
+    "use strict";
+    // ============================================================== 
+    // Newsletter
+    // ============================================================== 
+
+    //ct-visits
+    new Chartist.Line('#ct-visits', {
+        labels: ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'],
+        series: [
+            [5, 2, 7, 4, 5, 3, 5, 4]
+            , [3, 4, 6, 2, 4, 5, 3, 7],
+            [4, 3, 5, 6, 2, 5, 3, 4],
+            [2, 5, 2, 6, 4, 5, 2, 6]
+        ]
+    }, {
+        top: 0,
+        low: 1,
+        showPoint: true,
+        fullWidth: true,
+        plugins: [
+            Chartist.plugins.tooltip()
+        ],
+        axisY: {
+            labelInterpolationFnc: function (value) {
+                return (value / 1) + 'k';
+            }
+        },
+        showArea: true
+    });
+
+
+    var chart = [chart];
+
+    var sparklineLogin = function () {
+        $('#sparklinedash').sparkline([0, 5, 6, 10, 9, 12, 4, 9], {
+            type: 'bar',
+            height: '30',
+            barWidth: '4',
+            resize: true,
+            barSpacing: '5',
+            barColor: '#46a5d3'
+        });
+        $('#sparklinedash2').sparkline([0, 5, 6, 10, 9, 12, 4, 9], {
+            type: 'bar',
+            height: '30',
+            barWidth: '4',
+            resize: true,
+            barSpacing: '5',
+            barColor: '#fe5229'
+        });
+        $('#sparklinedash3').sparkline([0, 5, 6, 10, 9, 12, 4, 9], {
+            type: 'bar',
+            height: '30',
+            barWidth: '4',
+            resize: true,
+            barSpacing: '5',
+            barColor: '#feb929'
+        });
+        $('#sparklinedash4').sparkline([0, 5, 6, 10, 9, 12, 4, 9], {
+            type: 'bar',
+            height: '30',
+            barWidth: '4',
+            resize: true,
+            barSpacing: '5',
+            barColor: '#2dba1f'
+        });
+    }
+    var sparkResize;
+    $(window).on("resize", function (e) {
+        clearTimeout(sparkResize);
+        sparkResize = setTimeout(sparklineLogin, 500);
+    });
+    sparklineLogin();
+});
+
+
+
+
